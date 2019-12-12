b5fe4d63fd7f804e50052e6961d8a9a1db53c8c8
sizukutamago <tanaka@tam-bourine.co.jp>
2019-12-13 02:33:51 +0900

    本番環境でエラーが発生した場合indexに全て飛ばす

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if (env('APP_ENV') === 'production') {
+            return redirect()->route('index')->with('error', 'エラーが発生しました。最初からやり直してください');
+        }
         return parent::render($request, $exception);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <h1>ビンビンビンゴ！！！</h1>
+
+    @if (session('error'))
+        <div style="color: red;">
+            {{ session('error') }}
+        </div>
+    @endif
+
     <form action="{{ route('createRoom') }}" method="post">
         <button class="btn" type="submit">ビンゴルームを作成！</button>
         @csrf
